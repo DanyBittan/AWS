a651380062b4ec81333b680efd2873cbd589f1d1
DanyBi <danielbittang@gmail.com>
2023-02-07 13:14:27 +0100

    update

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -1,9 +1,23 @@
 #! /bin/bash
 STACK_NAME=awsbootstrap
+STACK_NAME_2=s3-bucket
 REGION=us-east-1
 CLI_PROFILE=default
 
 EC2_INSTANCE_TYPE=t2.micro
+
+AWS_ID=`aws sts get-caller-identity --query "Account" --output text`
+S3_BUCKET_NAME="${REGION}"-pipeline-"${AWS_ID}"
+
+echo -e "\n================== Desplegando setup.yml ====================="
+
+aws cloudformation deploy \
+	--region $REGION \
+	--profile $CLI_PROFILE \
+	--stack-name $STACK_NAME_2 \
+	--template-file setup.yml \
+	--no-fail-on-empty-changeset \
+	--parameter-override BucketName=$S3_BUCKET_NAME
 
 echo -e "\n================== Desplegando main.yml ====================="
 
